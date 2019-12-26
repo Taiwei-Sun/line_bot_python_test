558dfd81cee6ec22f89953e1e64ab12d51cdb246
root <Tai Wei Sun>
2019-12-26 04:46:53 +0000

    test1: add condition for response

--- quick_flask.py
+++ quick_flask.py
@@ -36,9 +36,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    clientMessage=event.message.text
+    clientMessageArray=clientMessage.split()
+    if clientMessageArray[0]=="Fudge":
+        clientMessage=clientMessage.replace("Fudge ","")
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=clientMessage))
 
 
 if __name__ == "__main__":
