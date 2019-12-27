1dccde4cb64993a19f8cc47d546b1f845c98431e
root <Tai Wei Sun>
2019-12-27 07:31:46 +0000

    add prayTmp in .gitignore

--- quick_flask.py
+++ quick_flask.py
@@ -10,10 +10,26 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import os
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('yjc2B9p8sbGN7jutAUPASpmJUE+AYwjq96P6ErBD7kGZ5P1BWC4aFeznWGQyqUBnzxdUUWonoUvu+oS9fo/Ox2J4I/bXgfv0KqLx4wk/0i8n+pShPRXkT809X3wsP31FEiJAi9dYW/V/GMUL5wS8LAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('48937fdda35c6bf04da439ff7fb9f5fc')
+
+prayDirectory="prayTmp"
+
+
+def createDirectory(dir):
+    if not os.path.exists(dir):
+        try:
+            os.mkdir(dir)
+        except OSError:
+            print ("Creation of the directory %s failed" % dir)
+        else:
+            print ("Successfully created the directory %s " % dir)
+
+
 
 
 @app.route("/callback", methods=['POST'])
@@ -39,14 +55,13 @@ def handle_message(event):
     clientMessage=event.message.text
     clientMessageArray=clientMessage.split()
     if clientMessageArray[0]=="Fudge":
-        if event.source.type == 'user':
-            clientMessage=clientMessage+"user id:"+event.source.user_id
-        elif event.source.type == 'group':
-            clientMessage=clientMessage+"group id:"+event.source.group_id
-        elif event.source.type == 'room':
-            clientMessage=clientMessage+"room id:"+event.source.room_id
         clientMessage=clientMessage.replace("Fudge ","")
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=clientMessage))
+    
+    if "禱告名單" in clientMessage:
+        createDirectory(prayDirectory)
+        
+        
 
 
 if __name__ == "__main__":
