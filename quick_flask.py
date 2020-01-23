c48df3bbc0d5d58366ac5fd9f762f61522e5eb4b
root <Tai Wei Sun>
2020-01-23 10:18:21 +0000

    test for personal use 06

--- quick_flask.py
+++ quick_flask.py
@@ -349,7 +349,7 @@ def handle_message(event):
         for i in range(int(messageLen/100)):
             tmpMessage=clientMessage[:100]
             clientMessage=clientMessage[100:]
-            modify_tables(userMessage[0],[userMessage[1][1],userMessage[1][2],userMessage[1][3]],[sourceID['user_id'],"'"+profile.display_name+"'","'"+tmpMessage+"'"],'i')
+            modify_tables(userMessage[0],[userMessage[1][1],userMessage[1][2],userMessage[1][3]],["'"+sourceID['user_id']+"'","'"+profile.display_name+"'","'"+tmpMessage+"'"],'i')
         
         modify_tables(userMessage[0],[userMessage[1][1],userMessage[1][2],userMessage[1][3]],["'"+sourceID['user_id']+"'","'"+profile.display_name+"'","'"+clientMessage+"'"],'i')
         sendMessage="Hi "+profile.display_name+", "+clientMessage #" It cannot be used for personal use"
